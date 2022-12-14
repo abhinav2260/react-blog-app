23caeb9565925b8ed4a9288e861920fccbf815e2
abhinav <2000520320004@ietlucknow.ac.in>
2022-12-14 21:12:19 +0530

    Updates

---
+++ precache-manifest.8cb0e9e418d6225e6fb13bbbcc987073.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7704086c668c1c4960a4",
+    "url": "/react-blog-app/static/css/main.d07b1201.chunk.css"
+  },
+  {
+    "revision": "7704086c668c1c4960a4",
+    "url": "/react-blog-app/static/js/main.7704086c.chunk.js"
+  },
+  {
+    "revision": "23b48cf1ef0fdd0010aa",
+    "url": "/react-blog-app/static/js/1.23b48cf1.chunk.js"
+  },
+  {
+    "revision": "2fb614e4f8b858af77f6",
+    "url": "/react-blog-app/static/js/runtime~main.2fb614e4.js"
+  },
+  {
+    "revision": "8c33b764c20f9baec4a5d04ff379b8b9",
+    "url": "/react-blog-app/index.html"
+  }
+];
