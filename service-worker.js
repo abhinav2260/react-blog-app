8c6526950f7ddb5c51fc12037291c63748488a2d
abhinav <2000520320004@ietlucknow.ac.in>
2022-12-14 21:20:38 +0530

    init upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react_blog_app/precache-manifest.14ad6a51ebc689dd6659fe7392035ecd.js"
+  "/react-blog-app/precache-manifest.8cb0e9e418d6225e6fb13bbbcc987073.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react_blog_app/index.html", {
+workbox.routing.registerNavigationRoute("/react-blog-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
